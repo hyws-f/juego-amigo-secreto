362356aac3ae7637f2f00a8a46fe892603b72185
hyws-f <cristopherbernal5422@gmail.com>
2025-08-09 01:47:19 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo() {
+    
+    let input = document.getElementById("nombreAmigo");
+    let nombre = input.value.trim(); // trim() quita espacios en blanco al inicio y final
+
+    
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return; 
+    }
+
+    
+    amigos.push(nombre);
+
+    
+    input.value = "";
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indice];
+
+    document.getElementById("resultado").innerHTML = "Amigo sorteado: " + amigoSorteado;
+}
